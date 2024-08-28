43e59b05d370ec3adbdd519256a99d2203ddfec3
akatsukiduck <marotimo016@gmail.com>
2024-08-28 01:16:26 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+let randomeize =  Math.floor(Math.random() * 4);
+console.log(randomeize);
+if(randomeize == 0){
+    console.log('A certain victory');
+}
+if(randomeize == 1){
+    console.log("not so certain victory");
+}
+if(randomeize == 2){
+    console.log("an uneasy victory");
+}
